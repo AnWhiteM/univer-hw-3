725db433f4b605fefe8e702c5dd0c316a218a016
Ivan Grivtsov <94605350+AnWhiteM@users.noreply.github.com>
2023-10-02 12:43:08 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Task 2--------------
+// const adminUser = "Ivan";
+// const correctPassword = "abcd1234";
+
+// if (adminUser === adminUser && correctPassword === correctPassword) {
+//   console.log("correct user");
+// } else if (adminUser || correctPassword === null) {
+//   console.log("no data");
+// } else {
+//   console.log("wrong credentials");
+// }
+
+// Task 3--------------
+
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number ${i}!`);
+//   i++;
+// }
+
+// Task 4--------------
+// function min(firstNumber, secondNumber) {
+//   if (firstNumber < secondNumber) {
+//     console.log(firstNumber);
+//   } else {
+//     console.log(secondNumber);
+//   }
+// }
+
+// min(2, 5);
+// min(10, 4);
+
+// Task5--------------
+// function isEmpty(obj) {
+//   for (var key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// var obj1 = {};
+// var obj2 = {key ="1"};
+
+// console.log(isEmpty(obj1));
+// console.log(isEmpty(obj2));
